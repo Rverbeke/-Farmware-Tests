8d37bd5a3723340307a61d81b658bb59d1704e26
Rverbeke <48277218+Rverbeke@users.noreply.github.com>
2019-06-26 14:38:28 +0200

    Added log messages

--- test.py
+++ test.py
@@ -2,7 +2,13 @@ from farmware_tools import device, get_config_value
 
 farmware_name = 'Tests'
 
-X = get_config_value(farmware_name, config_name='X')
-Y = get_config_value(farmware_name, config_name='Y')
+device.log("Fetching coordinates", messagetype="info")
 
+X = get_config_value(farmware_name, config_name='X')
+device.log("X coordinate recieved", messagetype="success")
+Y = get_config_value(farmware_name, config_name='Y')
+device.log("Y coordinate recieved", messagetype="success")
+
+device.log("attempting to move to inputed coordinates", messagetype="info")
 device.move_relative(X, Y, 0, 100)
+device.log("Movement successful", messagetype="success")
