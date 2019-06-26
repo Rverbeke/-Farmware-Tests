93acb20ee7a8832528fc02b69b3f8456c8da6463
Rverbeke <48277218+Rverbeke@users.noreply.github.com>
2019-06-26 15:15:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,9 @@
+#!/usr/bin/env python
+
+'''Hello Farmware
+A simple Farmware example that tells FarmBot to log a new message.
+'''
+
 from farmware_tools import device
 
 device.log(message='Hello Farmware!', message_type='success')
-
-
-"""
-from farmware_tools import device, get_config_value
-
-device.log(message="Begin Farmware tests", message_type="success")
-
-farmware_name = 'Tests'
-
-device.log(message="Fetching coordinates", message_type="info")
-X = get_config_value(farmware_name, config_name='X')
-device.log(message="X coordinate recieved", message_type="success")
-Y = get_config_value(farmware_name, config_name='Y')
-device.log(message="Y coordinate recieved", message_type="success")
-
-device.log(message="attempting to move to inputed coordinates", message_type="info")
-device.move_relative(X, Y, 0, 100)
-device.log(message="Movement successful", message_type="success")
-"""
