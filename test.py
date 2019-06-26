4181d9992779b5b8dfbf33221507e64b4da8afa9
Rverbeke <48277218+Rverbeke@users.noreply.github.com>
2019-06-26 16:29:34 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,11 @@
 A simple Farmware example that tells FarmBot to log a new message.
 '''
 
-from farmware_tools import device
+from farmware_tools import device, get_config_value
 
-device.log(message='Hello Farmware!', message_type='success')
+farmware_name = 'test'
+
+X = get_config_value(farmware_name, config_name=X)
+Y = get_config_value(farmware_name, config_name=Y)
+
+device.move_relative(X, Y, 0, 100)
