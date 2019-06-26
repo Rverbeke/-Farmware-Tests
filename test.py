38af3f25ba56622a12cf92fc9b04b714cc57f1c3
Rverbeke <48277218+Rverbeke@users.noreply.github.com>
2019-06-26 15:00:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from farmware_tools import device, get_config_value
 
-device.log("Begin Farmware tests", message_type="success")
+device.log(message="Begin Farmware tests", message_type="success")
 
 farmware_name = 'Tests'
 
-device.log("Fetching coordinates", message_type="info")
+device.log(message="Fetching coordinates", message_type="info")
 X = get_config_value(farmware_name, config_name='X')
-device.log("X coordinate recieved", message_type="success")
+device.log(message="X coordinate recieved", message_type="success")
 Y = get_config_value(farmware_name, config_name='Y')
-device.log("Y coordinate recieved", message_type="success")
+device.log(message="Y coordinate recieved", message_type="success")
 """
-device.log("attempting to move to inputed coordinates", message_type="info")
+device.log(message="attempting to move to inputed coordinates", message_type="info")
 device.move_relative(X, Y, 0, 100)
-device.log("Movement successful", message_type="success")
+device.log(message="Movement successful", message_type="success")
 """
