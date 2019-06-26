e4d55d3b08dbf165648ce9c578d020a28f87f3df
Rverbeke <48277218+Rverbeke@users.noreply.github.com>
2019-06-26 14:21:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from farmware_tools import device, get_config_value
 
 farmware_name = 'Tests'
 
-X = get_config_value(farmware_name, config_name='X_direction')
-Y = get_config_value(farmware_name, config_name='Y_direction')
+X = get_config_value(farmware_name, config_name='X')
+Y = get_config_value(farmware_name, config_name='Y')
 
 device.move_relative(X, Y, 0, 100)
