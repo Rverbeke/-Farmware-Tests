cbd4444b157f57a0471f5bc7860d71a79d1ce367
Rverbeke <48277218+Rverbeke@users.noreply.github.com>
2019-06-26 16:41:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ from farmware_tools import device, get_config_value
 
 farmware_name = 'test'
 
-X = get_config_value(farmware_name, config_name=X)
-Y = get_config_value(farmware_name, config_name=Y)
+X_pos = get_config_value(farmware_name, config_name=X)
+Y_pos = get_config_value(farmware_name, config_name=Y)
 device.log(message='farmbot is at {{ x }}, {{ y }}, {{ z }}')
 
-device.move_relative(X, Y, 0, 100)
+device.move_relative(X_pos, Y_pos, 0, 100)
 device.log(message='farmbot is at {{ x }}, {{ y }}, {{ z }}')
