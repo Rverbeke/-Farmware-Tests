feb3464b794f3818048ce68d1a8bdd013908def9
Rverbeke <48277218+Rverbeke@users.noreply.github.com>
2019-06-28 14:26:19 +0200

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
+'''
+Circle.
 
-'''Circle.
-
-Tells the farmbot to make a circle
+Makes the farmbot follow a pseudo-circle
 '''
 
 from farmware_tools import device, get_config_value
 import numpy as np
-
 
 farmware_name = 'Circle'
 
@@ -24,14 +22,14 @@ length = diameter * np.sin(np.array(np.pi/points))
 apothem = diameter/2 * np.cos(np.array(np.pi/points))
 angle = (points-2)*np.pi/points
 
-#Create angle value for each point
+#Create angle value for each point, relative to X axis
 angles = [0]
 i = 1
 while(i<points):
 	angles.append(angles[i-1] + np.pi - angle)
 	i+=1
 
-#Move to 1st point
+#Move to 0th point
 device.move_relative(-length/2, -apothem, 0, 100)
 
 #Move to each point
@@ -41,27 +39,4 @@ for current_angle in angles:
 	device.move_relative(next_X, next_Y, 0, 100)
 
 #Get back to original position
-
 device.move_absolute(device.assemble_coordinate(farmbot_X_coord, farmbot_Y_coord, 0), 100, device.assemble_coordinate(0, 0, 0))
-
-"""
-#Legacy code
-
-#Length of the polygon's sides
-length = diameter * np.sin(np.array(np.pi/points))
-
-#Angle for each side relative to X axis
-angles = [0]
-i = 1
-while(i<=points):
-	angles.append(angles[i-1] + 2*np.pi/points)
-	i+=1
-
-#Move the farmbot to new coordinates
-for angle in angles:
-	next_X = farmbot_X_coord + length * np.cos(np.array(angle))
-	next_Y = farmbot_Y_coord + length * np.sin(np.array(angle))
-	device.move_absolute(device.assemble_coordinate(next_X, next_Y, 0), 100, device.assemble_coordinate(0, 0, 0))
-	print((next_X, next_Y))
-device.move_absolute(device.assemble_coordinate(farmbot_X_coord, farmbot_Y_coord, 0), 100, device.assemble_coordinate(0, 0, 0))
-"""
